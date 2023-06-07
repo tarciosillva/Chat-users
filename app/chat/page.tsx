245ea04a699923b0ed709ee919e08f3efb47c843
Tarcio Sillva <tarciosillva7@gmail.com>
2023-06-07 13:10:38 -0300

    update chat

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import io from "socket.io-client";
-import { useState, useEffect } from "react";
+import { useState, useEffect, use } from "react";
 
 import Message from "@/app/components/Messages";
 import { BsFillSendFill } from "react-icons/bs";
@@ -22,7 +22,7 @@ interface payloadProps {
 
 interface userActive {
     id: string,
-    name: string
+    nome: string
 }
 
 export default function Chat() {
@@ -30,7 +30,6 @@ export default function Chat() {
     const userPhone = sessionStorage.getItem('userPhone')
     const socket = io('http://localhost:80');
     const [message, setMessage] = useState("");
-    const [onlineUsers, setOnlineUsers] = useState<userActive[]>([])
 
     const [messages, setMessages] = useState<messageProps[]>([]);
 
@@ -55,25 +54,18 @@ export default function Chat() {
     }, [users])
 
     useEffect(() => {
-        if (user) {
-            socket.emit('join', user.nome);
-        }
-    }, [user])
+        socket.emit('join');
+
+        socket.on("connect", () => {
+            console.log("SOCKET CONNECTED!", socket.id);
+        });
+    }, [])
 
     useEffect(() => {
         socket.on('msgToClient', (message: payloadProps) => {
             receivedMessageByServer(message);
         });
     }, [message, messages, user.nome]);
-
-    /*   useEffect(() => {
-          function receivedOnlineUsers(users: userActive[]) {
-              setOnlineUsers([...users]);
-          }
-          socket.on('onlineUsers', (users: userActive[]) => {
-              receivedOnlineUsers(users)
-          });
-      }, [user]); */
 
     function receivedMessageByServer(message: messageProps) {
         const newMessage: payloadProps = {
@@ -100,76 +92,63 @@ export default function Chat() {
 
     return (
         <div className="flex justify-center pt-2 h-full">
-            <div className="bg-white w-9/12 h-full rounded-lg">
+            <div className="bg-white w-6/12 h-full rounded-lg">
                 <div className="h-10 w-full bg-neutral-800 flex items-center justify-between pl-4 pr-4">
                     <p className="text-gray-100 text-lg">Bem vindo(a), {user?.nome}</p>
                 </div>
-                <div className="flex items-center">
-                    {/* Lista de usuários */}
-                    <div className="w-3/12 h-[88vh] bg-white  border-r-gray-300 border-r">
-                        {onlineUsers.map((userOnline, index) => (
-                            userOnline.name !== user.nome ? (
-                                <UserItem
-                                    key={index}
-                                    name={userOnline.name}
-                                />
-                            ) : (<></>)
+                <div className="w-full h-[88vh]">
+                    <div className="h-[83vh] overflow-y-auto">
+                        {messages.map((message, index) => (
+                            message.name === user.nome ? (
+                                <div className="w-full mt-5 flex justify-end pr-5" key={index}>
+                                    <Message
+                                        sender="you"
+                                        text={message.text}
+                                    />
+                                </div>
+                            ) : (
+                                <div className="w-full mt-5 pl-5" key={index}>
+                                    <Message
+                                        sender="other"
+                                        Name={message.name}
+                                        phone=""
+                                        text={message.text}
+                                    />
+                                </div>
+                            )
                         ))}
                     </div>
-                    <div className="w-9/12 h-[88vh]">
-                        <div>
-                            {messages.map((message, index) => (
-                                message.name === user.nome ? (
-                                    <div className="w-full mt-5 flex justify-end pr-5" key={index}>
-                                        <Message
-                                            sender="you"
-                                            text={message.text}
-                                        />
-                                    </div>
-                                ) : (
-                                    <div className="w-full mt-5 pl-5" key={index}>
-                                        <Message
-                                            sender="other"
-                                            Name={message.name}
-                                            phone=""
-                                            text={message.text}
-                                        />
-                                    </div>
-                                )
-                            ))}
-                        </div>
 
-                        <div className="
+                    <div className="
                             p-1 
-                            w-[56.3vw]
+                            w-6/12
                             bg-white 
                             text-zinc-700
                             text-sm
                             border-t-gray-300 
                             border-t
                             placeholder-gray-500
-                            bottom-11 
+                            bottom-5 
                             absolute
                             flex 
                             items-center 
                             justify-between
                         ">
-                            <input
-                                className="bg-transparent w-[56.3vw] p-3 text-sm text-slate-900 font-medium"
-                                type="text"
-                                name=""
-                                id=""
-                                placeholder="Escreva sua mensagem ..."
-                                value={message}
-                                onChange={(e) => setMessage(e.target.value)}
-                            />
-                            <button
-                                className="w-1/12 rounded-md bg-slate-900 p-2 flex items-center justify-center"
-                                onClick={() => sendMessage()}
-                            >
-                                <BsFillSendFill size={15} color="#ffff" />
-                            </button>
-                        </div>
+                        <input
+                            className="bg-transparent w-full p-3 text-sm text-slate-900 font-medium"
+                            type="text"
+                            name=""
+                            id=""
+                            placeholder="Escreva sua mensagem ..."
+                            value={message}
+                            onChange={(e) => setMessage(e.target.value)}
+                        />
+                        <button
+                            className="w-1/12 rounded-md bg-slate-900 p-2 flex items-center justify-center"
+                            onClick={() => sendMessage()}
+                        >
+                            <BsFillSendFill size={15} color="#ffff" />
+                        </button>
                     </div>
                 </div>
             </div>
--- app/components/Messages/index.tsx
+++ app/components/Messages/index.tsx
@@ -3,12 +3,12 @@ interface messageProps {
     sender: 'you' | 'other'
     Name?: string
     phone?: string
-    text:string
+    text: string
 }
 
 export default function Message(props: messageProps) {
     return (
-        <div className={`w-1/2 ${props.sender === "other" ? ('bg-slate-100') : ('bg-green-100')} rounded-md p-2`}>
+        <div className={`w-1/3 ${props.sender === "other" ? ('bg-slate-100') : ('bg-green-100')} rounded-md p-2`}>
             <div className="flex items-center justify-between">
                 <p className="text-slate-900 text-xs">
                     {props.sender === 'other' ? (props.phone) : ('')}
@@ -17,7 +17,7 @@ export default function Message(props: messageProps) {
                     {props.sender === 'other' ? (props.Name) : ('Você')}
                 </p>
             </div>
-            <div className="text-sm text-zinc-400 mt-2">
+            <div className="break-all text-sm text-zinc-400 mt-2">
                 {props.text}
             </div>
         </div>
