83f92afae462257f3a1627f47cbc94f9032fc6a9
Tarcio Sillva <tarciosillva7@gmail.com>
2023-06-07 06:55:28 -0300

    create root page

--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,93 @@
-import Image from 'next/image'
+"use client";
+import { useState, useEffect } from "react"
+import Link from 'next/link';
+import AcessElementForm from './components/acessElementForm';
+import useStore from "./utils/zustand.store";
+import { fetchDataGetAllUsers, fetchDataLoginAdmin } from "./service/usersAPI.service";
 
 export default function Home() {
+  const { users, setUsers } = useStore();
+
+  const [userPhone, setUserPhone] = useState<string>('')
+
+  const [adminPhone, setAdminPhone] = useState<string>('')
+  const [adminPassword, setAdminPassword] = useState<string>('')
+
+  useEffect(() => {
+    fetchDataGetAllUsers()
+      .then((response) => {
+        setUsers(response.result)
+      });
+  }, []);
+
+  const saveStorageUserPhone = () => {
+    if (!userPhone) {
+      setUserPhone('')
+      window.alert('Insira o número de telefone para entrar no chat.')
+      return
+    }
+    var userFind = users.find((user) => user.telefone == userPhone)
+    if (!userFind) {
+      setUserPhone('')
+      window.alert('Insira um número de telefone pré cadastrado na api')
+      return
+    }
+    sessionStorage.setItem('userPhone', userPhone)
+    window.location.href = "/chat"
+  }
+
+  const logginAdmin = () => {
+    fetchDataLoginAdmin({ telefone: adminPhone, password: adminPassword }).then((response) => {
+
+      if (response.access_token) {
+        sessionStorage.setItem('TOKEN', response.access_token)
+        window.location.href = "/admin"
+      } else {
+        window.alert('Telefone/Senha inválidos')
+      }
+    })
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className="h-screen w-full flex justify-center items-center">
+      <div className='w-1/2 border-r-gray-400 border-r'>
+        <div className='grid m-10 gap-6'>
+          <AcessElementForm
+            value={userPhone}
+            label='Insira o número de telefone para entrar no chat.'
+            onChange={setUserPhone}
+            placeholder='(00)9XXXXXXXX'
+          />
+          <button
+            onClick={() => saveStorageUserPhone()}
+
+            className="text-zinc-100 bg-gray-950 p-3 rounded-lg cursor-pointer text-center"
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            Acessar chat
+          </button>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className='w-1/2'>
+        <div className='grid m-10 gap-6'>
+          <AcessElementForm
+            value={adminPhone}
+            label='Insira o número de telefone admin'
+            onChange={setAdminPhone}
+            placeholder='(00)9XXXXXXXX'
+          />
+          <AcessElementForm
+            value={adminPassword}
+            label='Insira a senha admin'
+            onChange={setAdminPassword}
+            placeholder='***'
+          />
+          <button
+            onClick={() => logginAdmin()}
+            className="text-zinc-100 bg-gray-950 p-3 rounded-lg cursor-pointer text-center"
+          >
+            Acessar administrador
+          </button>
+        </div>
       </div>
     </main>
   )
